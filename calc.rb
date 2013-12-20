bdd05607e24c2568480095da5941281282fb855c
Stu Stein <stuartstein@users.noreply.github.com>
2013-12-19 20:29:31 -0800

    Update calc.rb
    I took out all those pesky if statements!

--- calc.rb
+++ calc.rb
@@ -1,9 +1,13 @@
-
 puts "Enter a number, a + or - sign, then another number"
 numb1 = gets.to_i
 operator = gets.chomp
-numb2 = gets.to_i
+numb2 = gets.chomp
 
+numb2 = operator + numb2
+
+puts "Your answer is: #{numb1 + numb2.to_i}"
+
+=begin
 if operator == "+"
 	puts "Result: #{numb1 + numb2}"
 elsif operator == "-"
@@ -11,5 +15,5 @@ elsif operator == "-"
 else
 	puts "Follow the instructions"
 end
-
+=end
 
